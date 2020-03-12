0a7d540cbeb2698d39c558cb683813cbeeb9c1e5
Luis PM <luisp237@gmail.com>
2020-03-12 12:40:02 -0400

    initial commit in heroku specific application

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('./api/server.js');
 
-const port = 4000;
+//make the port be assigned by ther Server
+//heroku will place the .PORT evnironment value on their server
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
